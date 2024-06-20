b1a8a85e1c04b017904cbc555220f2f03a1e400f
Danielfgb <dgomezbernal24@gmail.com>
2024-06-19 22:20:29 -0500

    Actualización-Archivos

--- Captura_RFI.py
+++ Captura_RFI.py
@@ -7,51 +7,41 @@
 # GNU Radio Python Flow Graph
 # Title: Captura RFI
 # Author: Daniel Gomez
-# GNU Radio version: 3.8.2.0
-
-from distutils.version import StrictVersion
-
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print("Warning: failed to XInitThreads()")
+# GNU Radio version: 3.10.8.0
 
 from PyQt5 import Qt
 from gnuradio import qtgui
-import sip
 from gnuradio import blocks
+from gnuradio import blocks, gr
 from gnuradio import fft
 from gnuradio.fft import window
 from gnuradio import gr
 from gnuradio.filter import firdes
 import sys
 import signal
+from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 from gnuradio import uhd
 import time
-import CSV
-import Control_Frecuencia
+import Captura_RFI_CSV as CSV  # embedded python block
+import Captura_RFI_Control_Frecuencia as Control_Frecuencia  # embedded python block
+import sip
 
-from gnuradio import qtgui
+
 
 class Captura_RFI(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "Captura RFI")
+        gr.top_block.__init__(self, "Captura RFI", catch_exceptions=True)
         Qt.QWidget.__init__(self)
         self.setWindowTitle("Captura RFI")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
         self.top_scroll_layout = Qt.QVBoxLayout()
         self.setLayout(self.top_scroll_layout)
         self.top_scroll = Qt.QScrollArea()
@@ -67,23 +57,24 @@ class Captura_RFI(gr.top_block, Qt.QWidget):
         self.settings = Qt.QSettings("GNU Radio", "Captura_RFI")
 
         try:
-            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-                self.restoreGeometry(self.settings.value("geometry").toByteArray())
-            else:
-                self.restoreGeometry(self.settings.value("geometry"))
-        except:
-            pass
+            geometry = self.settings.value("geometry")
+            if geometry:
+                self.restoreGeometry(geometry)
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
 
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate_0 = samp_rate_0 = 20000000
-        self.frec_inicial = frec_inicial = 70000000
-        self.frec_final = frec_final = 200000000
+        self.samp_rate = samp_rate = 1000000
+        self.frec_inicial = frec_inicial = 80000000
+        self.frec_final = frec_final = 300000000
+        self.Intervalo_Tiempo = Intervalo_Tiempo = 0.5
 
         ##################################################
         # Blocks
         ##################################################
+
         self.uhd_usrp_source_1 = uhd.usrp_source(
             ",".join(("", "")),
             uhd.stream_args(
@@ -92,11 +83,12 @@ class Captura_RFI(gr.top_block, Qt.QWidget):
                 channels=list(range(0,1)),
             ),
         )
+        self.uhd_usrp_source_1.set_samp_rate(samp_rate)
+        self.uhd_usrp_source_1.set_time_now(uhd.time_spec(time.time()), uhd.ALL_MBOARDS)
+
         self.uhd_usrp_source_1.set_center_freq(0, 0)
-        self.uhd_usrp_source_1.set_gain(70, 0)
         self.uhd_usrp_source_1.set_antenna('RX2', 0)
-        self.uhd_usrp_source_1.set_samp_rate(samp_rate_0)
-        self.uhd_usrp_source_1.set_time_unknown_pps(uhd.time_spec())
+        self.uhd_usrp_source_1.set_normalized_gain(1, 0)
         self.qtgui_vector_sink_f_0 = qtgui.vector_sink_f(
             1024,
             0,
@@ -104,15 +96,17 @@ class Captura_RFI(gr.top_block, Qt.QWidget):
             "Frecuencia MHz",
             "dB",
             "",
-            1 # Number of inputs
+            1, # Number of inputs
+            None # parent
         )
         self.qtgui_vector_sink_f_0.set_update_time(0.10)
-        self.qtgui_vector_sink_f_0.set_y_axis(-140, 2)
+        self.qtgui_vector_sink_f_0.set_y_axis((-140), 2)
         self.qtgui_vector_sink_f_0.enable_autoscale(False)
         self.qtgui_vector_sink_f_0.enable_grid(True)
         self.qtgui_vector_sink_f_0.set_x_axis_units("")
         self.qtgui_vector_sink_f_0.set_y_axis_units("")
         self.qtgui_vector_sink_f_0.set_ref_level(0)
+
 
         labels = ['', '', '', '', '',
             '', '', '', '', '']
@@ -132,19 +126,17 @@ class Captura_RFI(gr.top_block, Qt.QWidget):
             self.qtgui_vector_sink_f_0.set_line_color(i, colors[i])
             self.qtgui_vector_sink_f_0.set_line_alpha(i, alphas[i])
 
-        self._qtgui_vector_sink_f_0_win = sip.wrapinstance(self.qtgui_vector_sink_f_0.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_vector_sink_f_0_win)
+        self._qtgui_vector_sink_f_0_win = sip.wrapinstance(self.qtgui_vector_sink_f_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_vector_sink_f_0_win)
         self.fft_vxx_0 = fft.fft_vcc(1024, True, window.blackmanharris(1024), True, 1)
+        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_float*1024, 50000, True, 0 if "auto" == "auto" else max( int(float(0.1) * 50000) if "auto" == "time" else int(0.1), 1) )
         self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_gr_complex*1, 1024)
         self.blocks_nlog10_ff_0 = blocks.nlog10_ff(10, 1024, 0)
         self.blocks_multiply_const_xx_0 = blocks.multiply_const_cc(0.000976562, 1024)
-        self.blocks_message_debug_0 = blocks.message_debug()
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_float*1024, 'C:\\Users\\dfgom\\OneDrive\\Escritorio\\USRP\\RFI_Captura\\Salida\\Dat_crudos', False)
-        self.blocks_file_sink_0.set_unbuffered(False)
+        self.blocks_message_debug_0 = blocks.message_debug(True, gr.log_levels.info)
         self.blocks_complex_to_mag_squared_0 = blocks.complex_to_mag_squared(1024)
-        self.Control_Frecuencia = Control_Frecuencia.blk(frec_inicial=frec_inicial, frec_final=frec_final, intervalo_tiempo=0.2, ancho_banda=samp_rate_0)
-        self.CSV = CSV.CSVWriterBlock(filename=r"C:\Users\dfgom\OneDrive\Escritorio\USRP\RFI_Captura\Salida\CSV_Salida.csv")
-
+        self.Control_Frecuencia = Control_Frecuencia.blk(frec_inicial=frec_inicial, frec_final=frec_final, intervalo_tiempo=Intervalo_Tiempo, ancho_banda=samp_rate)
+        self.CSV = CSV.CSVWriterBlock(Filename_csv="CSV_Salida")
 
 
         ##################################################
@@ -155,10 +147,10 @@ class Captura_RFI(gr.top_block, Qt.QWidget):
         self.msg_connect((self.Control_Frecuencia, 'Frec_out'), (self.uhd_usrp_source_1, 'command'))
         self.connect((self.blocks_complex_to_mag_squared_0, 0), (self.blocks_nlog10_ff_0, 0))
         self.connect((self.blocks_multiply_const_xx_0, 0), (self.blocks_complex_to_mag_squared_0, 0))
-        self.connect((self.blocks_nlog10_ff_0, 0), (self.CSV, 0))
-        self.connect((self.blocks_nlog10_ff_0, 0), (self.blocks_file_sink_0, 0))
-        self.connect((self.blocks_nlog10_ff_0, 0), (self.qtgui_vector_sink_f_0, 0))
+        self.connect((self.blocks_nlog10_ff_0, 0), (self.blocks_throttle2_0, 0))
         self.connect((self.blocks_stream_to_vector_0, 0), (self.fft_vxx_0, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self.CSV, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self.qtgui_vector_sink_f_0, 0))
         self.connect((self.fft_vxx_0, 0), (self.blocks_multiply_const_xx_0, 0))
         self.connect((self.uhd_usrp_source_1, 0), (self.blocks_stream_to_vector_0, 0))
 
@@ -166,14 +158,17 @@ class Captura_RFI(gr.top_block, Qt.QWidget):
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "Captura_RFI")
         self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
         event.accept()
 
-    def get_samp_rate_0(self):
-        return self.samp_rate_0
+    def get_samp_rate(self):
+        return self.samp_rate
 
-    def set_samp_rate_0(self, samp_rate_0):
-        self.samp_rate_0 = samp_rate_0
-        self.uhd_usrp_source_1.set_samp_rate(self.samp_rate_0)
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.uhd_usrp_source_1.set_samp_rate(self.samp_rate)
 
     def get_frec_inicial(self):
         return self.frec_inicial
@@ -189,15 +184,18 @@ class Captura_RFI(gr.top_block, Qt.QWidget):
         self.frec_final = frec_final
         self.Control_Frecuencia.frec_final = self.frec_final
 
+    def get_Intervalo_Tiempo(self):
+        return self.Intervalo_Tiempo
+
+    def set_Intervalo_Tiempo(self, Intervalo_Tiempo):
+        self.Intervalo_Tiempo = Intervalo_Tiempo
+        self.Control_Frecuencia.intervalo_tiempo = self.Intervalo_Tiempo
 
 
 
 
 def main(top_block_cls=Captura_RFI, options=None):
 
-    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
     qapp = Qt.QApplication(sys.argv)
 
     tb = top_block_cls()
@@ -207,6 +205,9 @@ def main(top_block_cls=Captura_RFI, options=None):
     tb.show()
 
     def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
         Qt.QApplication.quit()
 
     signal.signal(signal.SIGINT, sig_handler)
@@ -216,11 +217,6 @@ def main(top_block_cls=Captura_RFI, options=None):
     timer.start(500)
     timer.timeout.connect(lambda: None)
 
-    def quitting():
-        tb.stop()
-        tb.wait()
-
-    qapp.aboutToQuit.connect(quitting)
     qapp.exec_()
 
 if __name__ == '__main__':
--- Captura_RFI_CSV.py
+++ Captura_RFI_CSV.py
@@ -1,18 +1,34 @@
+import os
 import csv
 import numpy as np
 from gnuradio import gr
 import pmt
+from datetime import datetime
 
 class CSVWriterBlock(gr.sync_block):
-    
-    def __init__(self, filename="output.csv"):
+
+    def __init__(self, Filename_csv="CSV_Salida"):
         gr.sync_block.__init__(
             self,
             name="CSV Writer Block",
-            in_sig=[(np.float32, 1024)],  # Tipo de datos de entrada (float) y tamaño de bloque (1024)
+            in_sig=[(np.float32, 1024)],  # Tipo de datos de entrada (float32) y tamaño de bloque (1024)
             out_sig=None,
         )
-        self.filename = filename
+
+        # Define la direccion donde se guarda el csv
+        output_dir = "Salida"
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        # Genera dia-mes-año_hora-min-s para el nombre del archivo csv
+        now = datetime.now()
+        date_time_str = now.strftime("%d-%m-%Y_%H-%M-%S")
+
+        # Construye el nombre del archivo
+        filename = f"{Filename_csv}_{date_time_str}.csv"
+        self.filename = os.path.join(output_dir, filename)
+        print(f"-- Medición guardada en: {self.filename} --")  
+
         self.file = open(self.filename, "w", newline="")
         self.writer = csv.writer(self.file)
         self.prev_freq_value = None  # Valor de frecuencia anterior
